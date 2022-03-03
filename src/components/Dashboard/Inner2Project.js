984260bd5f3f8da279dc97d0eb8136924cb2ec4a
holabisii <ajoseholabisi@gmail.com>
2022-03-03 03:30:18 -0500

    changed tasks pic to html

--- src/components/Dashboard/Inner2Project.js
+++ src/components/Dashboard/Inner2Project.js
@@ -7,10 +7,43 @@ const Inner2Project = () => {
                 <div className='root_Inner2Project'>
                         <br />
                         <br />
-                        <div>
-                                <img src="../../../trial1.png" alt="call" />
-                                <img src="../../../trial2.png" alt="call" />
-                                <img src="../../../trial3.png" alt="call" />
+                        <div className='root_Inner2Project_1'>
+                                <div className='root_Inner2Project_1_2'>
+                                        <div className='root_Inner2Project_1_2_1'>
+
+                                                <img src="../../../arrow_down.png" alt="call" />
+                                                <img src="../../../circle_orange.png" alt="call" />
+                                        
+                                        </div>
+                                        <div className='root_Inner2Project_1_2_2'>
+                                                <p>Task Remaining</p>
+                                                <h2>12/15</h2>
+                                        </div>
+                                </div>
+                                <div className='root_Inner2Project_1_2'>
+                                        <div className='root_Inner2Project_1_2_1'>
+                                        
+                                                <img src="../../../arrow_up.png" alt="call" />
+                                                <img src="../../../circle_green.png" alt="call" />
+                                        </div>
+                                        <div className='root_Inner2Project_1_2_2'>
+                                                <p>Task Left</p>
+                                                <h2>12/15</h2>
+                                        </div>
+                                
+                                </div>
+                                <div className='root_Inner2Project_1_2'>
+                                        <div className='root_Inner2Project_1_2_1'>
+                                        
+                                                <img src="../../../arrow_down.png" alt="call" />
+                                                <img src="../../../circle_red.png" alt="call" />
+                                        </div>
+                                        <div className='root_Inner2Project_1_2_2'>
+                                                <p>Task Done</p>
+                                                <h2>12/15</h2>
+                                        </div>
+                                
+                                </div>
                         </div>
                         <div className='root_Inner2Project_2'>
                                 <h2>Progress Stats</h2>
